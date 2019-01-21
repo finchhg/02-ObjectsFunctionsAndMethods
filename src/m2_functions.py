c8dd7d526af5c11b42bc15c21e01fc712bbf47aa
finchhg <finchhg@rose-hulman.edu>
2019-01-21 11:43:54 -0500

    m2

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -3,11 +3,11 @@ Practice DEFINING and CALLING
      FUNCTIONS
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Harrison Finch.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
-# TODO: 2.
+# done: 2.
 #   Allow this module to use the  rosegraphics.py  module by marking the
 #     src
 #   folder in this project as a "Sources Root", as follows:
@@ -21,6 +21,8 @@ import rosegraphics as rg
 
 
 def main():
+    i=hypotenuse(3,4)
+    print(i)
     """
     TESTS the functions that you will write below.
     You write the tests per the _TODO_s below.
@@ -28,7 +30,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 3a.  Define a function immediately below this _TODO_.
+# done: 3a.  Define a function immediately below this _TODO_.
 #   It takes two arguments that denote, for a right triangle,
 #   the lengths of the two sides adjacent to its right angle,
 #   and it returns the length of the hypotenuse of that triangle.
@@ -40,6 +42,12 @@ def main():
 #   to test whether you defined the function correctly.
 #
 ###############################################################################
+
+import math as m
+
+def hypotenuse(a,b):
+    hypotenuse = m.sqrt(a ** 2 + b ** 2)
+    return hypotenuse
 
 
 ###############################################################################
